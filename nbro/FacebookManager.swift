a66c122b0db26f933c10a9b8fc0421bc8ffcbf4e
Casper Storm Larsen <casperstorm@me.com>
2016-02-25 19:49:33 +0100

    Added error message when facebook login fails

--- nbro/FacebookManager.swift
+++ nbro/FacebookManager.swift
@@ -10,14 +10,16 @@ import Foundation
 import FBSDKLoginKit
 
 class FacebookManager {
-    class func logInWithReadPermissions(completion: (success: Bool) -> Void) {
+    class func logInWithReadPermissions(completion: (success: Bool, error: NSError?) -> Void) {
         let loginManager = FBSDKLoginManager()
         loginManager.logInWithReadPermissions(["public_profile"], fromViewController: nil, handler: {
             (result: FBSDKLoginManagerLoginResult?, error: NSError?) -> Void in
             if result?.token != nil {
-                completion(success: true)
+                completion(success: true, error: nil)
+            } else if error != nil {
+                completion(success: false, error: error)
             } else {
-                completion(success: false)
+                completion(success: false, error: nil)
             }
         })
     }
@@ -45,7 +47,7 @@ class FacebookManager {
     }
     
     class func user(completion: (user: User) -> Void) {
-        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "name"])
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "name, email"])
         graphRequest.startWithCompletionHandler({
             (connection, result, error) -> Void in
             guard let dict = result as? NSDictionary, user = User(dictionary: dict) else {
