e66eceb84038fba3b2973aa5398cbefed77e3a82
Casper Storm Larsen <casperstorm@me.com>
2016-02-26 11:19:48 +0100

    fixed small overlap in event text

--- nbro/Views/EventCell.swift
+++ nbro/Views/EventCell.swift
@@ -95,7 +95,7 @@ class TextView: UITextView, NSLayoutManagerDelegate {
     
     override func drawRect(rect: CGRect) {
         self.layoutManager.enumerateLineFragmentsForGlyphRange(NSMakeRange(0, self.text.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))) { (rect, usedRect, textContainer, glyphRange, stop) -> Void in
-            let inset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 5)
+            let inset = UIEdgeInsets(top: 0.03, left: 20, bottom: 0, right: 5)
             let path = UIBezierPath(rect: CGRect(x: usedRect.origin.x - inset.left, y: usedRect.origin.y - inset.top, width: usedRect.size.width + inset.left + inset.right + self.textContainerInset.left + self.textContainerInset.right, height: usedRect.size.height + inset.top + inset.bottom))
             UIColor(hex: 0x76edff, alpha: 0.55).setFill()
             path.fill()
