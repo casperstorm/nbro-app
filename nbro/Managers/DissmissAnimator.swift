5de0d3ffaa40420d507df63a52bca7c51b479402
Casper Storm Larsen <casperstorm@me.com>
2016-03-01 11:28:00 +0100

    added more effect to the dismiss modal animation

--- nbro/Managers/DissmissAnimator.swift
+++ nbro/Managers/DissmissAnimator.swift
@@ -32,10 +32,14 @@ extension DismissAnimator : UIViewControllerAnimatedTransitioning {
         let screenBounds = UIScreen.mainScreen().bounds
         let bottomLeftCorner = CGPoint(x: 0, y: screenBounds.height)
         let finalFrame = CGRect(origin: bottomLeftCorner, size: screenBounds.size)
+        toVC.view.transform = CGAffineTransformMakeScale(0.9, 0.9);
+
         
         UIView.animateWithDuration(
             transitionDuration(transitionContext), delay: 0.0, options: transitionContext.isInteractive() ? .CurveLinear : .CurveEaseInOut, animations: {
             fromVC.view.frame = finalFrame
+            toVC.view.transform = CGAffineTransformIdentity;
+            
             }, completion: { _ in
                 transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
             }
